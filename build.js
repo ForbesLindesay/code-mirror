ac5ad6cba07cbead29c88915c6b3475d11127075
Forbes Lindesay <forbes.lindesay@red-gate.com>
2013-10-15 10:59:33 +0100

    Try making the default theme work

--- build.js
+++ build.js
@@ -219,7 +219,8 @@ step('add JavaScript versions of CSS', function () {
     return theme.replace(/\.css$/, '');
   })
   var defaultCSS = readCSS('./codemirror.css');
-  write('./theme/default.js', 'require("insert-css")(' + JSON.stringify(defaultCSS) + ');');
+  write('./theme/default.js', 'require("insert-css")(' + JSON.stringify(defaultCSS) + ');',
+      'module.exports = require("./index.js").register("default");');
   for (var i = 0; i < themes.length; i++) {
     write('./theme/' + themeNames[i] + '.js', [
       'require("./default.js");',
